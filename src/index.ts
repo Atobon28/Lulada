ef7291049a25a7ae65b7f665632273d541fe4b7b
CrisJauregi <cristinajaureguibravo@gmail.com>
2025-04-06 00:06:05 -0500

    feat: scaffold inicial de componentes para Settings (archivos vacíos)

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import LoginForm from './Components/Login/CajaLogin';
 import LoginPage from "./Pages/LogIn/Login";
 
 
+
 customElements.define('root-component', RootComponent);
 customElements.define("caja-de-texto", CajaDeTexto);
 customElements.define("boton-login", BotonLogin);
 customElements.define("login-form", LoginForm);
 customElements.define("login-page", LoginPage);
+
