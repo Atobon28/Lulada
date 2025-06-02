5d91a59b6e0458a7424bd2546a1760247c189647
Danamelisa-g <danamelisag17@gmail.com>
2025-06-02 01:05:43 -0500

    feat(firebase)Firebase connection

---
+++ src/Services/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDntWN5qmsUdVZCZ7w7RVbJWOoULx9GVQg",
+  authDomain: "luladita-369e3.firebaseapp.com",
+  projectId: "luladita-369e3",
+  storageBucket: "luladita-369e3.firebasestorage.app",
+  messagingSenderId: "800273388542",
+  appId: "1:800273388542:web:94bf6ebc57493b478266af"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
