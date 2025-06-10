a720d0aae2437a27176cc12110ba175ceab77b2d
Danamelisa-g <danamelisag17@gmail.com>
2025-06-10 01:39:56 -0500

    breaking change(firebase,login and types)add diferesents changes and type to user

--- src/Services/firebase/firebase.ts
+++ src/Services/firebase/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +20,35 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const Auth = getAuth(app);
 
-export {db,Auth}
+
+const registerUser = async (email: string, password: string) => {
+  try {
+    console.log("Registering user with email:", email);
+    const userCredential = await createUserWithEmailAndPassword(
+      Auth,
+      email,
+      password
+    );
+    console.log(userCredential.user);
+    return { isRegistered: true, user: userCredential };
+  } catch (error) {
+    console.error(error);
+    return { isRegistered: false, error: error };
+  }
+};
+const loginUser = async (email: string, password: string) => {
+  try {
+    console.log("Logging in user with email:", email);
+    const userCredential = await signInWithEmailAndPassword(
+      Auth,
+      email,
+      password
+    );
+    console.log(userCredential.user);
+    return { isLoggedIn: true, user: userCredential };
+  } catch (error) {
+    console.error(error);
+    return { isLoggedIn: false, error: error };
+  }
+};
+export {db,Auth,registerUser, loginUser}
